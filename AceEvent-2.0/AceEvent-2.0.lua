567335209f4008f473f79e7d3326cc7f76258fa8
ckknight <ckknight@b14dd718-5223-4c98-bdc2-a81dac438608>
2006-07-08 02:34:22 +0000

    AceEvent - whoops, typo
    git-svn-id: https://repos.wowace.com/wow/ace2/tmp/trunk/Ace2@75 b14dd718-5223-4c98-bdc2-a81dac438608

--- AceEvent-2.0/AceEvent-2.0.lua
+++ AceEvent-2.0/AceEvent-2.0.lua
@@ -164,7 +164,7 @@ function AceEvent:TriggerDelayedEvent(event, delay, a1, a2, a3, a4, a5, a6, a7, 
 		delayRegistry = AceEvent.delayRegistry
 		AceEvent.frame:SetScript("OnUpdate", OnUpdate)
 	end
-	local t and compost:Acquire(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20) or {a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20}
+	local t = compost and compost:Acquire(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20) or {a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20}
 	t.event = event
 	t.time = GetTime() + delay
 	table.insert(self.delayRegistry, t)
