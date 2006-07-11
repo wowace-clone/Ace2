d44e1c8b7648128bc4862ac7c291ee3e40e2bec2
ckknight <ckknight@b14dd718-5223-4c98-bdc2-a81dac438608>
2006-07-11 01:26:04 +0000

    AceConsole - fix global x error
    git-svn-id: https://repos.wowace.com/wow/ace2/tmp/trunk/Ace2@141 b14dd718-5223-4c98-bdc2-a81dac438608

--- AceConsole-2.0/AceConsole-2.0.lua
+++ AceConsole-2.0/AceConsole-2.0.lua
@@ -884,7 +884,7 @@ local function handlerFunc(self, chat, msg, options)
 				
 				if type(options.step) == "number" and step > 0 then
 					local step = options.step
-					arg = math.floor((x - min) / step + 0.5) * step + min
+					arg = math.floor((arg - min) / step + 0.5) * step + min
 					if arg > max then
 						arg = max
 					elseif arg < min then
