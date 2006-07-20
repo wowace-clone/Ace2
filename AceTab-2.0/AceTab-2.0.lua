3a2d1c6882558c9ac454ac2598b960fa39ba010c
hyperactiveChipmunk <hyperactiveChipmunk@b14dd718-5223-4c98-bdc2-a81dac438608>
2006-07-20 21:52:04 +0000

    AceTab-2.0: - Bugfixifications. All known bugs squashed.
    git-svn-id: https://repos.wowace.com/wow/ace2/tmp/trunk/Ace2@228 b14dd718-5223-4c98-bdc2-a81dac438608

--- AceTab-2.0/AceTab-2.0.lua
+++ AceTab-2.0/AceTab-2.0.lua
@@ -5,7 +5,7 @@ Author(s): hyperactiveChipmunk (hyperactiveChipmunk@gmail.com)
 Website: http://www.wowace.com/
 Documentation: http://wiki.wowace.com/index.php/AceTab-2.0
 SVN: http://svn.wowace.com/root/trunk/Ace2/AceTab-2.0
-Description: Tab-completion functionality
+Description: A tab-completion library
 Dependencies: AceLibrary
 ]]
 
@@ -16,26 +16,26 @@ if not AceLibrary then error(MAJOR_VERSION .. " requires AceLibrary.") end
 if not AceLibrary:IsNewVersion(MAJOR_VERSION, MINOR_VERSION) then return end
 
 local AceHook, AceEvent
-local AceTab = {hookLock=true}
+local AceTab = {}
 local _G = getfenv()
 
 local hookedFrames = compost and compost:Acquire() or {}
 
 function AceTab:RegisterTabCompletion(descriptor, regex, compfunc, usagefunc, editframes)
-	self:argCheck(descriptor, 2, "string")
-	self:argCheck(regex, 3, "string", "table")
-	self:argCheck(compfunc, 4, "string", "function", "nil")
-	self:argCheck(usagefunc, 5, "string", "function", "nil")
-	self:argCheck(editframe, 6, "string", "table", "nil")
+	AceTab:argCheck(descriptor, 2, "string")
+	AceTab:argCheck(regex, 3, "string", "table")
+	AceTab:argCheck(compfunc, 4, "string", "function", "nil")
+	AceTab:argCheck(usagefunc, 5, "string", "function", "nil")
+	AceTab:argCheck(editframe, 6, "string", "table", "nil")
 
 	if type(regex) == "string" then regex = {regex} end
 
 	if type(compfunc) == "string" and type(self[compfunc]) ~= "function" then
-		self:error("Cannot register function %q; it does not exist", compfunc)
+		AceTab:error("Cannot register function %q; it does not exist", compfunc)
 	end
 
 	if type(usagefunc) == "string" and type(self[usagefunc]) ~= "function" then
-		self:error("Cannot register usage function %q; it does not exist", usagefunc)
+		AceTab:error("Cannot register usage function %q; it does not exist", usagefunc)
 	end
 
 	if not editframes then editframes = {"ChatFrameEditBox"} end
@@ -52,16 +52,16 @@ function AceTab:RegisterTabCompletion(descriptor, regex, compfunc, usagefunc, ed
 		end
 
 		if type(Gframe) ~= "table" or not Gframe.Show then
-			self:error("Cannot register frame %q; it does not exist", frame)
+			AceTab:error("Cannot register frame %q; it does not exist", frame)
 			frame = nil
 		end
 		
 		if frame then
 			if Gframe:GetFrameType() ~= "EditBox" then
-				self:error("Cannot register frame %q; it is not an EditBox", frame)
+				AceTab:error("Cannot register frame %q; it is not an EditBox", frame)
 				frame = nil
 			else
-				if not self.hookLock and not self:IsHooked(GFrame, "OnTabPressed") then
+				if AceEvent:IsFullyInitialized() and not self:IsHooked(GFrame, "OnTabPressed") then
 					self:HookScript(Gframe, "OnTabPressed")
 				else
 					hookedFrames[frame] = true
@@ -70,61 +70,57 @@ function AceTab:RegisterTabCompletion(descriptor, regex, compfunc, usagefunc, ed
 		end
 	end
 	
-	if not self.registry[descriptor] then
-		self.registry[descriptor] = Compost and Compost:Acquire() or {}
+	if not AceTab.registry[descriptor] then
+		AceTab.registry[descriptor] = Compost and Compost:Acquire() or {}
 	end
 	
-	if not self.registry[descriptor][self] then
-		self.registry[descriptor][self] = Compost and Compost:Acquire() or {}
+	if not AceTab.registry[descriptor][self] then
+		AceTab.registry[descriptor][self] = Compost and Compost:Acquire() or {}
 	end
-	foo = Geditframes
-	self.registry[descriptor][self] = {patterns = regex, compfunc = compfunc,  usage = usagefunc, frames = editframes}
+
+	AceTab.registry[descriptor][self] = {patterns = regex, compfunc = compfunc,  usage = usagefunc, frames = editframes}
 	
 	if not AceEvent and AceLibrary:HasInstance("AceEvent-2.0") then
 		external(AceTab, "AceEvent-2.0", AceLibrary("AceEvent-2.0"))
 	end
 	if AceEvent then
 		if not self.finalHook then
-			self:RegisterEvent("AceEvent_FullyInitialized", "AceEvent_FullyInitialized", true)
+			AceTab:RegisterEvent("AceEvent_FullyInitialized", "AceEvent_FullyInitialized", true)
 		end
 	end
 end
 
 function AceTab:IsTabCompletionRegistered(descriptor)
-	self:argCheck(descriptor, 2, "string")
-	if self.registry[descriptor] and self.registry[descriptor][self] then
-		return true, self.registry[descriptor][self].completion
+	AceTab:argCheck(descriptor, 2, "string")
+	if AceTab.registry[descriptor] and AceTab.registry[descriptor][self] then
+		return true, AceTab.registry[descriptor][self].completion
 	end
 	return false, nil
 end
 
 function AceTab:UnregisterTabCompletion(descriptor)
-	self:argCheck(descriptor, 2, "string")
-	if self.registry[description] and self.registry[description][self] then
-		self.registry[descriptor][self].completion = nil
+	AceTab:argCheck(descriptor, 2, "string")
+	if AceTab.registry[description] and AceTab.registry[description][self] then
+		AceTab.registry[descriptor][self].completion = nil
 	else
-		self:error("Cannot unregister a tab completion that you have not registered.")
+		AceTab:error("Cannot unregister a tab completion that you have not registered.")
 	end
 end
 
-local function LCS(strings) --returns Least Common Substring.  Yoinked wholesale from Tem.
-	local len = 0
-	local numStrings = table.getn(strings)
-	for _, s in strings do
-		if type(s) == "string" then
-			len = string.len(s) > len and string.len(s) or len
-		end
+local GCS
+GCS = function(s1, s2)
+	if not s1 and not s2 then return end
+	if not s1 then s1 = s2 end
+	if not s2 then s2 = s1 end
+	local s1len, s2len = string.len(s1), string.len(s2)
+	if s2len < s1len then
+		s1, s2 = s2, s1
 	end
-	
-	for i = 1, len do
-		local c = string.lower(string.sub(strings[1], i, i))
-		for j = 2, numStrings do
-			if string.lower(string.sub(strings[j], i, i)) ~= c then
-				return string.sub(strings[1], 0, i-1)
-			end
-		end
+	if string.find(string.lower(s2), string.lower(s1)) then
+		return s1
+	else
+		return GCS(string.sub(s1, 1, -2), s2)
 	end
-	return strings[1]
 end
 
 function AceTab:OnTabPressed()
@@ -138,8 +134,9 @@ function AceTab:OnTabPressed()
 
 	local text = string.sub(this:GetText(), 0, pos) or ""
 
-	local left = string.find(string.sub(text, 1, pos), "%w+$") or pos
-	if not left then return self.hooks[this].OnTabPressed.orig() end
+	local left = string.find(string.sub(text, 1, pos), "%S+$") or pos
+	
+	if not left or left == 1 and string.sub(text, 1, 1) == "/" then return self.hooks[this].OnTabPressed.orig() end
 
 	local _, _, word = string.find(string.sub(text, left, pos), "(%S+)")
 	if not word then word = "" end
@@ -148,55 +145,55 @@ function AceTab:OnTabPressed()
 	local matches = compost and compost:Erase() or {}
 	local numMatches = 0
 	
-	for desc, entry in pairs(self.registry) do
+	for desc, entry in pairs(AceTab.registry) do
 		for _, s in pairs(entry) do
 			for _, f in s.frames do
 				if _G[f] == this then
 					for _, regex in ipairs(s.patterns) do
 						matches[desc] = compost and compost:Erase() or {}
-						s.cands = compost and compost:Erase() or {}
+						local cands = compost and compost:Erase() or {}
 						if string.find(string.sub(text, 1, left), regex) then
-							s.compfunc(s.cands, text, left-1)
+							s.compfunc(cands, string.sub(text, 1, left-1) .. string.sub(text, 1, left-1+string.len(word)), left-1)
 						end
-						for _, cand in ipairs(s.cands) do
+						for _, cand in ipairs(cands) do
 							if string.find(string.lower(cand), string.lower(word), 1, 1) == 1 then
 								table.insert(matches[desc], cand)
 								numMatches = numMatches + 1
 							end
 						end
-						if matches[desc][1] then matches[desc].usage = s.usage end
+						matches[desc].usage = s.usage
 					end
 				end
 			end
 		end
 	end
 	
-	if numMatches == 0 then return self.hooks[this].OnTabPressed.orig() end
 	this:HighlightText(left-1, left + string.len(word)-1)
+
 	if numMatches == 1 then
 		local _, c = next(matches)
 		this:Insert(c[1])
 		this:Insert(" ")
 	else
-		local matchlist = compost and compost:Erase() or {}
+		local gcs
 		for h, c in pairs(matches) do
+			local u = c.usage
+			local gcs2
 			if next(c) then
-				if not c.usage then
-					print(h..":")
-					for _, m in ipairs(c) do
-						print(m)
-					end
-				else
-					c.usage(c, LCS(c), text)
+				if not u then print(h..":") end
+				for _, m in ipairs(c) do
+					if not u then print(m) end
+					gcs2 = GCS(gcs2, m)
 				end
 			end
+			gcs = GCS(gcs, gcs2)
+			if u then u(c, gcs2, string.sub(text, 1, left-1)) end
 		end
-		this:Insert(LCS(matchlist))
+		this:Insert(gcs)
 	end
 end
 
 function AceTab:AceEvent_FullyInitialized()
-	self.hookLock = nil
 	for frame in pairs(hookedFrames) do
 		self:HookScript(_G[frame], "OnTabPressed")
 	end
