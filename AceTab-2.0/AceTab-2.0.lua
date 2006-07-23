09be4f9b4066fdef534bb0b34ce8203e71158582
hyperactiveChipmunk <hyperactiveChipmunk@b14dd718-5223-4c98-bdc2-a81dac438608>
2006-07-23 04:48:47 +0000

    AceTab-2.0: -Nuther typo >8P
    git-svn-id: https://repos.wowace.com/wow/ace2/tmp/trunk/Ace2@241 b14dd718-5223-4c98-bdc2-a81dac438608

--- AceTab-2.0/AceTab-2.0.lua
+++ AceTab-2.0/AceTab-2.0.lua
@@ -91,7 +91,7 @@ end
 
 function AceTab:IsTabCompletionRegistered(descriptor)
 	self:argCheck(descriptor, 2, "string")
-	return self.registry[descriptor] and self.registry[descriptor][self] end
+	return self.registry[descriptor] and self.registry[descriptor][self]
 end
 
 function AceTab:UnregisterTabCompletion(descriptor)
