95bff227553e35a141376b87282f01b375ac8967
ckknight <ckknight@b14dd718-5223-4c98-bdc2-a81dac438608>
2006-07-24 07:27:53 +0000

    AceConsole - fixed invalid option for range with step
    git-svn-id: https://repos.wowace.com/wow/ace2/tmp/trunk/Ace2@248 b14dd718-5223-4c98-bdc2-a81dac438608

--- AceConsole-2.0/AceConsole-2.0.lua
+++ AceConsole-2.0/AceConsole-2.0.lua
@@ -1057,7 +1057,7 @@ local function handlerFunc(self, chat, msg, options)
 					good = true
 				end
 				
-				if type(options.step) == "number" and options.step > 0 then
+				if good and type(options.step) == "number" and options.step > 0 then
 					local step = options.step
 					arg = math.floor((arg - min) / step + 0.5) * step + min
 					if arg > max then
