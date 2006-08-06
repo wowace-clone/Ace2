2d594c8762324e1a5baec8d7fbba07063ab5c5da
ckknight <ckknight@b14dd718-5223-4c98-bdc2-a81dac438608>
2006-08-06 03:13:06 +0000

    AceAddon - classpool creation is now wrapped in a pcall.
    git-svn-id: https://repos.wowace.com/wow/ace2/tmp/trunk/Ace2@355 b14dd718-5223-4c98-bdc2-a81dac438608

--- AceAddon-2.0/AceAddon-2.0.lua
+++ AceAddon-2.0/AceAddon-2.0.lua
@@ -339,7 +339,7 @@ function AceAddon.prototype:ToString()
 end
 
 AceAddon.new = function(self, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20)
-	local class = AceOO.Classpool(self, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20)
+	local class = AceAddon:pcall(AceOO.Classpool, self, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20)
 	return class:new()
 end
 
