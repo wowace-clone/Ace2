394ebcec454bac3f0dc38e62b548d11f9bd8fec9
ckknight <ckknight@b14dd718-5223-4c98-bdc2-a81dac438608>
2006-09-22 08:37:53 +0000

    AceComm - bugfix. probably.
    git-svn-id: https://repos.wowace.com/wow/ace2/tmp/trunk/Ace2@541 b14dd718-5223-4c98-bdc2-a81dac438608

--- AceComm-2.0/AceComm-2.0.lua
+++ AceComm-2.0/AceComm-2.0.lua
@@ -2202,7 +2202,8 @@ local function activate(self, oldLib, oldDeactivate)
 			end
 		end
 		local old_FCFDropDown_LoadChannels = FCFDropDown_LoadChannels
-		function FCFDropDown_LoadChannels(...)
+		function FCFDropDown_LoadChannels(a1,a2,a3,a4,a5,a6,a7,a8,a9,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20)
+			local arg = {a1,a2,a3,a4,a5,a6,a7,a8,a9,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20}
 			if self.hooks.FCFDropDown_LoadChannels then
 				return self.hooks.FCFDropDown_LoadChannels(self, old_FCFDropDown_LoadChannels, unpack(arg))
 			else
