f0972ede7035d516182278bd325221f9ad053338
ckknight <ckknight@b14dd718-5223-4c98-bdc2-a81dac438608>
2006-12-12 01:20:29 +0000

    .AceConsole-2.0 - now accepts numbers as AceOptions args keys.
    git-svn-id: https://repos.wowace.com/wow/ace2/tmp/trunk/Ace2@771 b14dd718-5223-4c98-bdc2-a81dac438608

--- AceConsole-2.0/AceConsole-2.0.lua
+++ AceConsole-2.0/AceConsole-2.0.lua
@@ -368,11 +368,14 @@ local function findTableLevel(self, options, chat, text, index, passTable)
 			end
 		end
 		if not disabled then
-			local next = work[index] and work[index]:lower()
+			local next = work[index] and string.lower(work[index])
+			local next_num = tonumber(next)
 			if next then
 				for k,v in pairs(options.args) do
 					local good = false
-					if k:lower() == next then
+					if string.lower(k) == next then
+						good = true
+					elseif k == next_num then
 						good = true
 					elseif type(v.aliases) == "table" then
 						for _,alias in ipairs(v.aliases) do
@@ -688,21 +691,27 @@ local function validateOptions(options, position, baseOptions, fromPass)
 			return '"args" must be a table', position
 		end
 		for k,v in pairs(options.args) do
-			if type(k) ~= "string" then
-				return '"args" keys must be strings', position
-			elseif k:find("%s") then
-				return string.format('"args" keys must not include spaces. %q is not appropriate.', k), position
-			elseif k:len() == 0 then
-				return '"args" keys must not be 0-length strings.', position
+			if type(k) ~= "number" then
+				if type(k) ~= "string" then
+					return '"args" keys must be strings or numbers', position
+				elseif k:find("%s") then
+					return string.format('"args" keys must not include spaces. %q is not appropriate.', k), position
+				elseif k:len() == 0 then
+					return '"args" keys must not be 0-length strings.', position
+				end
 			end
 			if type(v) ~= "table" then
-				return '"args" values must be tables', position and position .. "." .. k or k
+				if type(k) == "number" then
+					return '"args" values must be tables', position and position .. "[" .. k .. "]" or "[" .. k .. "]"
+				else
+					return '"args" values must be tables', position and position .. "." .. k or k
+				end
 			end
 			local newposition
-			if position then
-				newposition = position .. ".args." .. k
+			if type(k) == "number" then
+				newposition = position and position .. ".args[" .. k .. "]" or "args[" .. k .. "]"
 			else
-				newposition = "args." .. k
+				newposition = position and position .. ".args." .. k or "args." .. k
 			end
 			local err, pos = validateOptions(v, newposition, baseOptions, options.pass)
 			if err then
