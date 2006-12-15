ea5911a59e2381d2c7a15dd0c937127844c7d9be
ckknight <ckknight@b14dd718-5223-4c98-bdc2-a81dac438608>
2006-12-15 06:52:43 +0000

    .AceEvent-2.0 - event blacklist now set to work pre-PLAYER_LOGIN
    git-svn-id: https://repos.wowace.com/wow/ace2/tmp/trunk/Ace2@795 b14dd718-5223-4c98-bdc2-a81dac438608

--- AceEvent-2.0/AceEvent-2.0.lua
+++ AceEvent-2.0/AceEvent-2.0.lua
@@ -839,7 +839,7 @@ local function activate(self, oldLib, oldDeactivate)
 	ALL_EVENTS = self.ALL_EVENTS
 	FAKE_NIL = self.FAKE_NIL
 	RATE = self.RATE
-	local inPlw = false
+	local inPlw = not self.playerLogin
 	local blacklist = {
 		UNIT_INVENTORY_CHANGED = true,
 		BAG_UPDATE = true,
