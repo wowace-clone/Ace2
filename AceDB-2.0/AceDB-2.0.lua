ddf6608787c70cfc9c9744308fdfb84ce0c36d3d
ckknight <ckknight@b14dd718-5223-4c98-bdc2-a81dac438608>
2007-04-22 01:34:03 +0000

    .AceDB-2.0 - potentially fix bug where the char database is nil
    git-svn-id: https://repos.wowace.com/wow/ace2/tmp/trunk/Ace2@923 b14dd718-5223-4c98-bdc2-a81dac438608

--- AceDB-2.0/AceDB-2.0.lua
+++ AceDB-2.0/AceDB-2.0.lua
@@ -846,7 +846,9 @@ function AceDB:InitializeDB(addonName)
 	else
 		CrawlForDeserialization(_G[db.name])
 	end
-	if type(_G[db.charName]) == "table" then
+	if type(_G[db.charName]) ~= "table" then
+		_G[db.charName] = {}
+	else
 		CrawlForDeserialization(_G[db.charName])
 	end
 	rawset(db, 'raw', _G[db.name])
