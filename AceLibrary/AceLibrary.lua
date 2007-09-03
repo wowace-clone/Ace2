a993937e5ae878fad6d70ed89f41e3afd9431839
ckknight <ckknight@b14dd718-5223-4c98-bdc2-a81dac438608>
2007-09-03 02:03:36 +0000

    .AceLibrary - upgrade to LibStub r2
    git-svn-id: https://repos.wowace.com/wow/ace2/tmp/trunk/Ace2@1037 b14dd718-5223-4c98-bdc2-a81dac438608

--- AceLibrary/AceLibrary.lua
+++ AceLibrary/AceLibrary.lua
@@ -27,7 +27,7 @@ if previous and not previous:IsNewVersion(ACELIBRARY_MAJOR, ACELIBRARY_MINOR) th
 do
 	-- LibStub is a simple versioning stub meant for use in Libraries.  http://www.wowace.com/wiki/LibStub for more info
 	-- LibStub is hereby placed in the Public Domain -- Credits: Kaelten, Cladhaire, ckknight, Mikk, Ammo, Nevcairiel, joshborke
-	local LIBSTUB_MAJOR, LIBSTUB_MINOR = "LibStub", 47864
+	local LIBSTUB_MAJOR, LIBSTUB_MINOR = "LibStub", 2  -- NEVER MAKE THIS AN SVN REVISION! IT NEEDS TO BE USABLE IN ALL REPOS!
 	local LibStub = _G[LIBSTUB_MAJOR]
 
 	if not LibStub or LibStub.minor < LIBSTUB_MINOR then
